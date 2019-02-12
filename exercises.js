c6c79beca4215618df267317c5e1aa77843d3424
unknown <fr.heggeness@gmail.com>
2019-02-12 10:05:19 -1000

    q6

--- exercises.js
+++ exercises.js
@@ -119,7 +119,16 @@ console.log(stockCar);
    the return value in a variable named `completePerson`. Use `console.log` three times to print the entire object, just 
    the value at `name`, and just the value at `age`.
  */
-
+var plainPerson = {};
+function buildPerson(person, nameString, age){
+    plainPerson.name = nameString;
+    plainPerson.age = age;
+    return person;
+}
+var completePerson = buildPerson(plainPerson, 'Bob', 21);
+console.log(completePerson);
+console.log(completePerson.name);
+console.log(completePerson.age);
 
 /*
 7. Display values of objects that are inside an array
